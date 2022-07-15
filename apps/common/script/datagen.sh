5a73935429ea74693e5d406a3c5682bf21b6f44e
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2022-07-15 17:05:28 +0200

    [apps] Add fp-exp benchmark

--- apps/common/script/datagen.sh
+++ apps/common/script/datagen.sh
@@ -28,6 +28,12 @@ then
   args="${@:2}"
   # Use default values if args is not set
   [ -z "$args" ] && args="112 7"
+elif [[ $1 == *"exp"* ]]
+then
+  # Convolutions need args to be passed along
+  args="${@:2}"
+  # Use default values if args is not set
+  [ -z "$args" ] && args="4"
 else
   # Other program datagens do not need any arguments
   args=
